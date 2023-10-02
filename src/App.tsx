a5c0ac5c352a9ffb39b58d4b32d27905b74820c0
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-10-02 11:35:49 +0700

    demo condition rendering

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Greeting name="Bun" greetingMsg="Hello!" />
+      <Greeting name="Bun" greetingMsg="Hello!" isLoggedIn={false} />
     </div>
   )
 }
