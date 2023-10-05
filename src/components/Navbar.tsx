396367d34ad96a2e08f0270ca90c625b6f38a442
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-10-05 11:23:10 +0700

    NavLink

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
 import classes from './Navbar.module.css'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className={classes.navbar}>
-      <p>Navbar</p>
-      <button className={classes.login}>Login</button>
+      <div className={classes.menu}>
+        <p>Navbar</p>
+        <NavLink className={({ isActive }) => (isActive ? classes.active : classes.inactive)} to="/">
+          Feed
+        </NavLink>
+      </div>
+      <div className={classes.menu}>
+        <NavLink className={({ isActive }) => (isActive ? classes.active : classes.inactive)} to="/profile">
+          Profile
+        </NavLink>
+        <button className={classes.login}>Login</button>
+      </div>
     </nav>
   )
 }
