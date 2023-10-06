55a347c1b744094f4850e6d551b4483f27e432a2
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-10-06 14:14:55 +0700

    condition rendering on AuthContext

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -8,7 +8,9 @@ interface IAuthProviderProps {
 
 interface IAuthContextType {
   isLoggedIn: boolean
+  username: string | null
   login: (username: string, password: string) => Promise<void>
+  logout: () => void
 }
 
 const AuthContext = createContext<IAuthContextType | null>(null)
@@ -22,9 +24,11 @@ export const useAuth = () => {
 }
 
 const token = localStorage.getItem('token')
+const user = localStorage.getItem('username')
 
 const AuthProvider = ({ children }: IAuthProviderProps) => {
   const [isLoggedIn, setIsLoggedin] = useState<boolean>(!!token)
+  const [username, setUsername] = useState<string | null>(user)
 
   const login = async (username: string, password: string) => {
     const loginBody: LoginDTO = { username, password }
@@ -35,13 +39,19 @@ const AuthProvider = ({ children }: IAuthProviderProps) => {
       })
 
       localStorage.setItem('token', res.data.accessToken)
+      localStorage.setItem('username', username)
       setIsLoggedin(true)
+      setUsername(username)
     } catch (err) {
       throw new Error('Invalid username or password')
     }
   }
 
-  return <AuthContext.Provider value={{ isLoggedIn, login }}>{children}</AuthContext.Provider>
+  const logout = () => {
+    // * ????
+  }
+
+  return <AuthContext.Provider value={{ isLoggedIn, username, login, logout }}>{children}</AuthContext.Provider>
 }
 
 export default AuthProvider
